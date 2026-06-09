933af08c704c21ba3db264328e87e5e868ae4a9d
{dabin5682} <dabin5682@gmail.com>
2026-06-09 09:34:55 +0900

    feat : 초기 환경설정

---
+++ src/main/java/com/nhnacademy/flyschedule/config/DataGoKrApiProperties.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.flyschedule.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "data-go-kr.api")
+public class DataGoKrApiProperties {
+
+    private String url;
+    private String serviceKey;
+}
---
+++ src/test/java/com/nhnacademy/flyschedule/SpringAiFlySchedule002041ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.flyschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFlySchedule002041ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
