ed061b0332ee184a7dc679257afb86f7c664f1a0
{dabin5682} <dabin5682@gmail.com>
2026-06-09 13:59:45 +0900

    feat : DataParserAgent 추가

---
+++ src/test/java/com/nhnacademy/flyschedule/tool/DataParserAgentTest.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.flyschedule.tool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class DataParserAgentTest {
+    private static final DateTimeFormatter API_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
+    private static final DateTimeFormatter INPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Autowired
+    private DataParserAgent dataParserAgent;
+
+    @Test
+    void parse() {
+        String date = dataParserAgent.parseDate("내일");
+        assertNotNull(date);
+        log.info("date: {}", date);
+        assertEquals(LocalDate.now().plusDays(1).format(API_DATE_FORMAT),date);
+
+    }
+
+    @Test
+    void parseInput() {
+        String date = dataParserAgent.parseDate("2026-10-20");
+        assertNotNull(date);
+        log.info("date: {}", date);
+        assertEquals("20261020",date);
+    }
+}
