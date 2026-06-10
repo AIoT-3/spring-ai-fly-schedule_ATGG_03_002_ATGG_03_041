890d24d5086150ad9373568ccb1c7c08567fec9a
yjKang227 <hellcodedev@gmail.com>
2026-06-10 09:55:45 +0900

    feat: AirlineCodeAgent 기능 구현 및 테스트 코드

---
+++ src/test/java/com/nhnacademy/flyschedule/service/agent/AirlineCodeAgentTest.java
@@ -0,0 +1,64 @@
+package com.nhnacademy.flyschedule.service.agent;
+
+import com.nhnacademy.flyschedule.dto.resposne.AirlineInfoResponse;
+import com.nhnacademy.flyschedule.service.TagoApiService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AirlineCodeAgentTest {
+
+    @Mock
+    private TagoApiService tagoApiService;
+
+    private AirlineCodeAgent airlineCodeAgent;
+
+    @BeforeEach
+    void setUp() {
+        when(tagoApiService.getAirlineInfoList())
+                .thenReturn(List.of(
+                        new AirlineInfoResponse("대한항공", "KAL"),
+                        new AirlineInfoResponse("아시아나항공", "AAR"),
+                        new AirlineInfoResponse("제주항공", "JJA")
+                ));
+
+        airlineCodeAgent = new AirlineCodeAgent(tagoApiService);
+        airlineCodeAgent.init();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "대한항공, KAL",
+            "아시아나항공, AAR",
+            "제주항공, JJA"
+    })
+    void testGetAirlineCode(String input, String expected) {
+        assertEquals(expected, airlineCodeAgent.getAirlineCode(input));
+    }
+
+    @Test
+    void testGetAirlineCode_InvalidInput() {
+        assertAll(
+                () -> assertThrows(
+                        IllegalArgumentException.class,
+                        () -> airlineCodeAgent.getAirlineCode("")
+                ),
+                () -> assertThrows(
+                        IllegalArgumentException.class,
+                        () -> airlineCodeAgent.getAirlineCode(null)
+                )
+        );
+    }
+}
