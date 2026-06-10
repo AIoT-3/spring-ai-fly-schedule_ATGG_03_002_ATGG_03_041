4c5c29a8813d7ed4f42ef946f03889b8b0e3ec9d
{dabin5682} <dabin5682@gmail.com>
2026-06-10 10:15:27 +0900

    feat: AirportTool 기능 컨트롤러에 추가

---
+++ src/main/java/com/nhnacademy/flyschedule/controller/McpController.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.flyschedule.controller;
+
+import com.nhnacademy.flyschedule.dto.resposne.AirportInfoResponse;
+import com.nhnacademy.flyschedule.tool.AirportInfoTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mcp")
+public class McpController {
+    private final AirportInfoTool airportInfoTool;
+
+    @GetMapping("/airports")
+    public ResponseEntity<List<AirportInfoResponse>> getAirports() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(airportInfoTool.getAirportInfo());
+    }
+
+
+}
