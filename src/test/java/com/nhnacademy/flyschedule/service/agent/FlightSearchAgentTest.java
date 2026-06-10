c79dd90a9fe086665857f3ac8b899a20cec39fa6
yjKang227 <hellcodedev@gmail.com>
2026-06-10 14:09:17 +0900

    refactor: import문 최적화

--- src/test/java/com/nhnacademy/flyschedule/service/agent/FlightSearchAgentTest.java
+++ src/test/java/com/nhnacademy/flyschedule/service/agent/FlightSearchAgentTest.java
@@ -14,7 +14,7 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- src/test/java/com/nhnacademy/flyschedule/service/agent/TimeFilterAgentTest.java
+++ src/test/java/com/nhnacademy/flyschedule/service/agent/TimeFilterAgentTest.java
@@ -9,7 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class TimeFilterAgentTest {
