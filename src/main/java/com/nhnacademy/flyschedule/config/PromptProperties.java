535c13516d32b4ab57a27402e71fd453757af09b
yjKang227 <hellcodedev@gmail.com>
2026-06-15 15:33:02 +0900

    fix: ConfigurationProperties 클래스 record 로 변경 & ConfigurationPropertiesScan 방식으로 스캔 변경

--- src/main/java/com/nhnacademy/flyschedule/config/PromptProperties.java
+++ src/main/java/com/nhnacademy/flyschedule/config/PromptProperties.java
@@ -1,13 +1,10 @@
 package com.nhnacademy.flyschedule.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Getter
-@Setter
+
 @ConfigurationProperties(prefix = "app.prompt.flight-search")
-public class PromptProperties {
-    private String systemPath;
-    private String userPath;
-}
+public record PromptProperties (
+    String systemPath,
+    String userPath
+) {}
