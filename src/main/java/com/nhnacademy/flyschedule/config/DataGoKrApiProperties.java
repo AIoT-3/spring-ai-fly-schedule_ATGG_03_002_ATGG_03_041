97d4c2f5a0ee66de869735fd874b2cebdd5aeda5
yjKang227 <hellcodedev@gmail.com>
2026-06-15 15:33:02 +0900

    fix: ConfigurationProperties 클래스 record 로 변경 & ConfigurationPropertiesScan 방식으로 스캔 변경

--- src/main/java/com/nhnacademy/flyschedule/config/DataGoKrApiProperties.java
+++ src/main/java/com/nhnacademy/flyschedule/config/DataGoKrApiProperties.java
@@ -1,13 +1,9 @@
 package com.nhnacademy.flyschedule.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Data
-@Component
 @ConfigurationProperties(prefix = "data-go-kr.api")
-public class DataGoKrApiProperties {
-    private String url;
-    private String serviceKey;
-}
+public record DataGoKrApiProperties (
+    String url,
+    String serviceKey
+) {}
--- src/main/java/com/nhnacademy/flyschedule/config/PromptConfig.java
+++ src/main/java/com/nhnacademy/flyschedule/config/PromptConfig.java
@@ -2,6 +2,9 @@ package com.nhnacademy.flyschedule.config;
 
 import com.nhnacademy.flyschedule.service.ai.prompt.FlightSearchPrompt;
 import com.nhnacademy.flyschedule.service.ai.prompt.PromptResourceLoader;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +19,16 @@ public class PromptConfig {
             PromptResourceLoader promptResourceLoader
     ) {
         return new FlightSearchPrompt(
-                promptResourceLoader.load(promptProperties.getSystemPath()),
-                promptResourceLoader.load(promptProperties.getUserPath())
+                promptResourceLoader.load(promptProperties.systemPath()),
+                promptResourceLoader.load(promptProperties.userPath())
         );
+    }
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                .maxMessages(100)
+                .build();
     }
 }
